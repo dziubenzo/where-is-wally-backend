db99ab5abe74575bb7dc270fa7b1aac6c35fc841
dziubenzo <dziubenzo@gmail.com>
2024-04-04 17:24:23 +0200

    Build get_all_posts route

--- controllers/levelController.js
+++ controllers/levelController.js
@@ -4,9 +4,13 @@ const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
 
 exports.get_all_levels = asyncHandler(async (req, res, next) => {
-  return res.json({
-    status: 'TODO',
-  });
+  const allLevels = await Level.find({}).lean().exec();
+  if (!allLevels.length) {
+    return res.status(404).json({
+      message: 'No levels found.',
+    });
+  }
+  return res.json(allLevels);
 });
 
 exports.post_create_level = asyncHandler(async (req, res, next) => {
